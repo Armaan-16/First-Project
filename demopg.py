7f44ef9c8e522267f63d67815200da8f7530148a
Armaan-16 <mishraarmaan999@gmail.com>
2025-10-06 10:12:51 +0530

    learning

--- demopg.py
+++ demopg.py
@@ -1,6 +1,15 @@
+from xmlrpc.client import boolean
 print("Hello, World!")
 # import pyttsx3
 # pyttsx3.speak("Bikaar")
-a=1
-b=3
-print(a+b)
+a=1.5 # this is a float
+b=3 # this is an integer
+n="armaan" # this is a string
+print(a+b) 
+z=1==1 #  this is a boolean expression
+print(z)
+print(type(n)) # this will give the type of variable
+print(type(a))
+print(not(True))
+t=float(b) # this changes the type of b to float
+print(t)
